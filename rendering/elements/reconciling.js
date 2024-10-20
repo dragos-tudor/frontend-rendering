5e35b8825fc3d697348149bb5ee75b50df00732c
Dragos Tudor <dragos.tudor@gmail.com>
2024-10-20 11:58:16 +0000

    refactor(rendering): rename elements with keys folder.

--- rendering/elements/reconciling.js
+++ rendering/elements/reconciling.js
@@ -2,15 +2,15 @@ import { renderElement } from "./rendering.js"
 import { unrenderElement } from "./unrendering.js"
 import { replaceElement } from "./replacing.js"
 import { updateElement } from "./updating.js"
-import { getReconcilingType, ReconcilingTypes } from "../element-reconcilings/getting.js"
+import { getReconciliationType, ReconciliationTypes } from "../reconciliations/getting.js"
 
 export const reconcileElement = (elem, $elem, $parent) =>
 {
-  switch(getReconcilingType(elem, $elem)) {
-    case ReconcilingTypes.render: return renderElement(elem, $parent)
-    case ReconcilingTypes.replace: return [replaceElement(renderElement(elem, $parent), $elem), unrenderElement($elem)]
-    case ReconcilingTypes.update: return updateElement(elem, $elem)
-    case ReconcilingTypes.unrender:return unrenderElement($elem)
+  switch(getReconciliationType(elem, $elem)) {
+    case ReconciliationTypes.render: return renderElement(elem, $parent)
+    case ReconciliationTypes.replace: return [replaceElement(renderElement(elem, $parent), $elem), unrenderElement($elem)]
+    case ReconciliationTypes.update: return updateElement(elem, $elem)
+    case ReconciliationTypes.unrender:return unrenderElement($elem)
     default: return []
   }
 }
