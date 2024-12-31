b5ae685a44224e8b9bdfe3ede417c861008d76f2
Dragos Tudor <dragos.tudor@gmail.com>
2024-12-31 11:57:12 +0000

    change(html): change html logging params order.

--- rendering-html/elements/logging.js
+++ rendering-html/elements/logging.js
@@ -1,3 +1,3 @@
 import { getHtmlName } from "./getting.js"
 
-export const logHtmlElement = ($elem, $parent, message, props, logger) => logger($elem, message, "elem:", getHtmlName($elem), "props:", props, "parent:", $parent && getHtmlName($parent))
+export const logHtmlElement = ($elem, $parent, message, props, logger) => logger(`${message} elem:`, getHtmlName($elem), "props:", props, "parent:", $parent && getHtmlName($parent), $elem)
