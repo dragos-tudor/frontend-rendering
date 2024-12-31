1eae81dcb246be40dc881d597538960b8768dc55
Dragos Tudor <dragos.tudor@gmail.com>
2024-12-31 11:54:36 +0000

    refactor: add missing rendering text logging.

--- rendering/elements/rendering.js
+++ rendering/elements/rendering.js
@@ -7,15 +7,19 @@ import { throwError } from "../errors/throwing.js"
 import { enableIgnoring } from "../ignores/enabling.js"
 import { enableLogging } from "../loggers/enabling.js"
 import { getElementNS } from "./getting.js"
-import { logElementOrText } from "./logging.js"
+import { logElement, logText } from "./logging.js"
 
 export const renderElement = (elem, $parent) =>
 {
-  if (isJsxText(elem)) return renderHtmlText(elem, $parent)
+  if (isJsxText(elem)) {
+    const $text = renderHtmlText(elem, $parent)
+    logText($text, "render")
+    return $text
+  }
 
   throwError(validateHtmlElement($parent))
-  throwError(validateJsxElement(elem))
   throwError(validateHtmlTagName(getJsxName(elem)))
+  throwError(validateJsxElement(elem))
   const props = getJsxProps(elem)
   const $elem = renderHtmlElement(getJsxName(elem), getElementNS(elem, $parent), $parent)
 
@@ -25,6 +29,6 @@ export const renderElement = (elem, $parent) =>
   enableIgnoring($elem, $parent)
   enableLogging($elem, $parent)
   storeJsxElement($elem, elem)
-  logElementOrText($elem, "render")
+  logElement($elem, "render")
   return $elem
 }
