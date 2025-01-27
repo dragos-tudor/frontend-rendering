5efa021720ee6471fa9e0ef30c03500d57f7607b
Dragos Tudor <dragos.tudor@gmail.com>
2025-01-27 15:58:22 +0200

    react(tests): use testing library for react tests.

--- performance.react.test.jsx
+++ performance.react.test.jsx
@@ -1,63 +1,53 @@
 // deno-lint-ignore-file no-global-assign
 /** @jsxImportSource  /react */
 import { assert } from "/asserts.ts"
-import { unstable_act as act } from "/react.js"
-import { createRoot } from "/react-dom.js"
-import { DOMLibraryUrl } from "./rendering-html/parsers/registering.js"
+import { render } from "/react-testing.js"
+import { registerLinkeDomParser } from "./mod.js"
 
-const dom = await import(DOMLibraryUrl)
-const { DOMParser } = dom
-Object.assign(globalThis, { ...dom, Event: globalThis.Event, EventTarget: globalThis.EventTarget })
-Object.assign(globalThis, { IS_REACT_ACT_ENVIRONMENT: true })
+await registerLinkeDomParser()
 window = globalThis
 
 Deno.test("use components => test rendering performance [react]", async (t) =>
 {
-  const getDocument = () => new DOMParser().parseFromString("<main></main>")
+  const getDocument = () => new DOMParser().parseFromString("<body><main></main></body>", "text/html")
   const B = (props) => <c x={props.x} $y={props.y}>{props.z}</c>
   const A = (props) => <>{props.children}</>
 
-  await t.step("no elements => append 3000 elements => operation ran fast [react]", async () => {
-    const elem = getDocument().querySelector("main")
-    const root = createRoot(elem)
+  await t.step("no elements => append 3000 elements => operation ran fast [react]", () =>
+  {
+    const root = getDocument()
+    const container = root.querySelector("main")
 
     const start = performance.now()
-    act(() => root.render(<A>{new Array(3000).fill(<B x={1} $y={true} z={"abc"}></B>)}</A>))
-    await Promise.resolve()
-
+    render(<A>{new Array(3000).fill(0).map((_, index) => <B key={index} x={1} $y={true} z={"abc"}></B>)}</A>, {container})
     const end = performance.now()
+
     assert(end - start < 1000, "Slow performance appending 3000 html elements")
   })
 
-  await t.step("2000 elements => update elements => operation ran fast [react]", async () => {
-
-    const elem = getDocument().querySelector("main")
-    const root = createRoot(elem)
-
-    act(() => root.render(<A>{new Array(2000).fill(<B x={1}></B>)}</A>))
-    await Promise.resolve()
+  await t.step("2000 elements => update elements => operation ran fast [react]", () =>
+  {
+    const root = getDocument()
+    const container = root.querySelector("main")
+    const screen = render(<A>{new Array(2000).fill(0).map((_, index) => <B key={index} x={1}></B>)}</A>, {container})
 
     const start = performance.now()
-    act(() => root.render(<A>{new Array(2000).fill(<B x={2}></B>)}</A>))
-    await Promise.resolve()
-
+    screen.rerender(<A>{new Array(2000).fill(0).map((_, index) => <B key={index} x={2}></B>)}</A>, {container})
     const end = performance.now()
+
     assert(end - start < 1000, "Slow performance updating 2000 html elements")
   })
 
-  await t.step("3000 elements => remove elements => operation ran fast [react]", async () => {
-    const elem = getDocument().querySelector("main")
-    const root = createRoot(elem)
-
-    act(() => root.render(<A>{new Array(3000).fill(<B x={1}></B>)}</A>))
-    await Promise.resolve()
+  await t.step("3000 elements => remove elements => operation ran fast [react]", () =>
+  {
+    const root = getDocument()
+    const container = root.querySelector("main")
+    const screen = render(<A>{new Array(3000).fill(0).map((_, index) => <B key={index} x={1}></B>)}</A>, {container})
 
     const start = performance.now()
-    act(() => root.render(<A></A>))
-    await Promise.resolve()
-
+    screen.rerender(<A></A>, {container})
     const end = performance.now()
+
     assert(end - start < 1000, "Slow performance removing 3000 html elements")
   })
-
 })
