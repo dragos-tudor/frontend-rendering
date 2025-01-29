78b8b93dcfa679824961193476f056fcb3d702a8
Dragos Tudor <dragos.tudor@gmail.com>
2025-01-29 17:18:06 +0200

    fix(logging): correct html nodes logging.

--- rendering-html/texts/logging.js
+++ rendering-html/texts/logging.js
@@ -1,4 +1,4 @@
 import { getHtmlName } from "../elements/getting.js"
 import { getHtmlText } from "./getting.js"
 
-export const logHtmlText = ($text, $parent, message, logger) => logger(`${message} text:`, getHtmlText($text), "parent:", $parent && getHtmlName($parent), $text)
+export const logHtmlText = ($text, $parent, message, logger) => logger($text, `${message} text:`, getHtmlText($text), "parent:", $parent && getHtmlName($parent))
