0d5d5a295ee1f5c7ab59a485bfbd7b1c0bea2296
Dragos Tudor <dragos.tudor@gmail.com>
2025-01-29 18:01:25 +0200

    extend(effects): export set initial func effect function.

--- index.js
+++ index.js
@@ -719,7 +719,7 @@ const Suspense = ({ suspending = true, fallback, children })=>{
 const getService = (services, name)=>services?.[name];
 const getServices = (elem)=>elem.ownerDocument.__services;
 export { getEffects as getEffects };
-export { setEffects as setEffects, setInitialEffect as setInitialEffect };
+export { setEffects as setEffects, setInitialEffect as setInitialEffect, setInitialFuncEffect as setInitialFuncEffect };
 export { useEffect as useEffect };
 export { dispatchEvent as dispatchEvent };
 export { setHtmlEventHandler as setHtmlEventHandler };
--- rendering-effects/public.js
+++ rendering-effects/public.js
@@ -1,3 +1,3 @@
 export { getEffects } from "./effects/getting.js"
-export { setEffects, setInitialEffect } from "./effects/setting.js"
+export { setEffects, setInitialEffect, setInitialFuncEffect } from "./effects/setting.js"
 export { useEffect } from "./effects/using.js"
